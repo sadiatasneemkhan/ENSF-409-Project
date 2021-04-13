a85b29717e91671d2cce946c6c0a1f651ad46cb6
Etienne <etienne.lagace@shaw.ca>
2021-04-13 01:11:13 -0600

    added documentation for OrderForm.java

--- edu/ucalgary/ensf409/OrderForm.java
+++ edu/ucalgary/ensf409/OrderForm.java
@@ -11,8 +11,9 @@ import java.io.*;
 import java.util.Vector;
 
 /**
- * OrderForm is a simple class that produces the formatted order form in a .txt format. 
- * It contains private variables for each part of the form and a Boolean variable.
+ * OrderForm is a simple class that produces the formatted order form in a .txt
+ * format. It contains private variables for all necessary information outputed
+ * to the .txt file.
  */
 
 public class OrderForm {
@@ -27,32 +28,53 @@ public class OrderForm {
     // Constructor
     public OrderForm() {
     }
-    
+
     /**
      * @summary Fills OrderForm class with results from user input
      * 
-     * @description Uses stored user input values and SQL library 
-     * to initialize all the fields of the OrderForm class 
+     * @description Uses stored user input values and SQL library to initialize all
+     *              the fields of the OrderForm class
      * 
-     * @param f, a, ids, p, m, t, b
+     * @param f
+     * @param amount
+     * @param ID
+     * @param price
+     * @param manufacturer
+     * @param tyoe
+     * @param overflow
      * 
      * @return
      */
-    public OrderForm(String f, int a, Vector<String> ids, Vector<Integer> p, String m, String t, Boolean b) {
-        this.furniture = f;
-        this.type = t;
-        this.manufacturer = m;
-        this.overflow = b;
-        this.amount = a;
-        this.ID = ids;
+    public OrderForm(String filing, int amount, Vector<String> ID, Vector<Integer> price, String manufacturer,
+            String type, Boolean overflow) {
+        this.furniture = filing;
+        this.type = type;
+        this.manufacturer = manufacturer;
+        this.overflow = overflow;
+        this.amount = amount;
+        this.ID = ID;
 
-        int l = p.size();
+        int l = price.size();
         for (int i = 0; i < l; i++) {
-            this.price += p.get(i);
+            this.price += price.get(i);
         }
     }
 
     // Generate order form
+    /**
+     * @summary Generates an order form for the user
+     * 
+     * @description Uses FileWriter class to write to an output file named
+     *              order.txt. Faculty name, contact, and date written and left
+     *              blank for the user to fill. Furniture type, category, and price
+     *              of order are outputted if possible and an "order could not be
+     *              fulfilled" text is ouputted if overflow was detected from the
+     *              overflow boolean
+     * 
+     * @param
+     * 
+     * @return
+     */
     public void generateOrder() throws IOException {
         try {
             File myOutput = new File("order.txt");
@@ -63,21 +85,29 @@ public class OrderForm {
             myWriter.write("Contact:\n");
             myWriter.write("Date:\n\n");
 
+            // Output furniture type, category, and price of order
             myWriter.write("Original Request: " + this.type + " " + this.furniture + ", " + this.amount + "\n\n");
 
+            // ID vector is check for emptiness to know whether IDs for furniture were found
+            // or not
             if (!ID.isEmpty()) {
                 myWriter.write("Items Ordered\n");
+
+                // Iterates through ID vector to get every ID
                 for (int i = 0; i < this.ID.size(); i++) {
                     myWriter.write("ID: " + this.ID.get(i) + "\n");
                 }
                 myWriter.write("Total Price: $" + this.price);
 
+                // checks for overflow for correct formatting of the order.txt file
                 if (overflow) {
                     myWriter.write("\n\n");
                 }
-                
+
             }
 
+            // checks for overflow to know whether or not an "order could not be fulfilled"
+            // message should be sent
             if (overflow) {
                 myWriter.write("Complete order could not be fulfilled based on current inventory.\n");
                 myWriter.write("Try reducing the amount of furniture requested or visit any of these manufacturers:\n");
@@ -95,59 +125,186 @@ public class OrderForm {
     }
 
     // Getters
+    /**
+     * @summary Returns furniture variable
+     * 
+     * @description Returns furniture private String variable from class OrderForm
+     * 
+     * @param
+     * 
+     * @return furniture
+     */
     public String getFurniture() {
         return this.furniture;
     }
 
+    /**
+     * @summary Returns type variable
+     * 
+     * @description Returns type private String variable from class OrderForm
+     * 
+     * @param
+     * 
+     * @return type
+     */
     public String getType() {
         return this.type;
     }
 
+    /**
+     * @summary Returns manufacturer variable
+     * 
+     * @description Returns manufacturer private String variable from class
+     *              OrderForm
+     * 
+     * @param
+     * 
+     * @return manufacturer
+     */
     public String getManufacturer() {
         return this.manufacturer;
     }
 
+    /**
+     * @summary Returns overflow variable
+     * 
+     * @description Returns overflow private Boolean variable from class OrderForm
+     * 
+     * @param
+     * 
+     * @return overflow
+     */
     public Boolean getOverflow() {
         return this.overflow;
     }
 
+    /**
+     * @summary Returns amount variable
+     * 
+     * @description Returns amount private Int variable from class OrderForm
+     * 
+     * @param
+     * 
+     * @return amount
+     */
     public int getAmount() {
         return this.amount;
     }
 
+    /**
+     * @summary Returns price variable
+     * 
+     * @description Returns price private Int variable from class OrderForm
+     * 
+     * @param
+     * 
+     * @return price
+     */
     public int getPrice() {
         return this.price;
     }
 
+    /**
+     * @summary Returns ID variable
+     * 
+     * @description Returns ID private Vector<String> variable from class OrderForm
+     * 
+     * @param
+     * 
+     * @return ID
+     */
     public Vector<String> getID() {
         return this.ID;
     }
 
     // Setters
+    /**
+     * @summary Sets furniture variable
+     * 
+     * @description Sets furniture private String variable from class OrderForm
+     * 
+     * @param furniture
+     * 
+     * @return
+     */
     public void setFurniture(String furniture) {
         this.furniture = furniture;
     }
 
+    /**
+     * @summary Sets type variable
+     * 
+     * @description Sets type private String variable from class OrderForm
+     * 
+     * @param type
+     * 
+     * @return
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * @summary Sets manufacturer variable
+     * 
+     * @description Sets manufacturer private String variable from class OrderForm
+     * 
+     * @param manufacturer
+     * 
+     * @return
+     */
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
 
+    /**
+     * @summary Sets overflow variable
+     * 
+     * @description Sets overflow private Boolean variable from class OrderForm
+     * 
+     * @param overflow
+     * 
+     * @return
+     */
     public void setOverflow(Boolean overflow) {
         this.overflow = overflow;
     }
 
+    /**
+     * @summary Sets amount variable
+     * 
+     * @description Sets amount private Int variable from class OrderForm
+     * 
+     * @param amount
+     * 
+     * @return
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * @summary Sets price variable
+     * 
+     * @description Sets price private Int variable from class OrderForm
+     * 
+     * @param price
+     * 
+     * @return
+     */
     public void setPrice(int price) {
         this.price = price;
     }
 
+    /**
+     * @summary Sets ID variable
+     * 
+     * @description Sets ID private Vector<String> variable from class OrderForm
+     * 
+     * @param ID
+     * 
+     * @return
+     */
     public void setID(Vector<String> ID) {
         this.ID = ID;
     }
