a639fd715e38ef2f5d764dc0c6503892acd61881
ancaicedo <77050076+ancaicedo@users.noreply.github.com>
2021-04-13 21:26:55 -0600

    Added final tests

--- InventoryTest.java
+++ InventoryTest.java
@@ -1,20 +1,25 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-
+import edu.ucalgary.ensf409.*;
+import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
 
 
 public class InventoryTest {
+
+    public final String USERNAME = "acaicedo";
+    public final String PASSWORD = "Am201849538";
+
     
     public InventoryTest(){
     }
 
     @Test
-
     public void test1Chair(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         int[] expected = {150, 10000, 400, 250, 225};
         ArrayList<Integer> a = new ArrayList<>();
@@ -37,10 +42,10 @@ public class InventoryTest {
 
         assertTrue("Getting 1 chair of any type is not working properly", Arrays.equals(expected,actual));        
     }
-    @Test
 
+    @Test
     public void test1Desk(){ 
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         int[] expected = {400, 300, 100};
         ArrayList<Integer> a = new ArrayList<>();
@@ -60,9 +65,8 @@ public class InventoryTest {
     } 
     
     @Test
-
     public void test1Lamp(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         int[] expected = {10, 30, 20};
         ArrayList<Integer> a = new ArrayList<>();
@@ -85,7 +89,7 @@ public class InventoryTest {
 
     @Test
     public void test1Filing(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         int[] expected = {100, 200, 300};
         ArrayList<Integer> a = new ArrayList<>();
@@ -105,82 +109,65 @@ public class InventoryTest {
     }
 
     @Test
-
-    public void test2TaskChairs(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+    public void testMaxTaskChairs(){
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(150);
-        expected.add(10000);
 
-        testSQL.populateFurniture("chair", "Task", 2); 
+        testSQL.populateFurniture("chair", "Task", 1); 
+        System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
         assertTrue("Getting 2 task chairs of any type is not working properly", expected.equals(actual));        
     }
 
     @Test
-
-    public void test2MeshChairs(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+    public void testMaxMeshChairs(){
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(225);
-        expected.add(10000);
 
-        testSQL.populateFurniture("chair", "Mesh", 2); 
+        testSQL.populateFurniture("chair", "Mesh", 1);
+        System.out.println(testSQL.chain.getOverflow());  
         Vector<Integer> actual = testSQL.chain.getPrice();
 
         assertTrue("Getting 2 Mesh chairs of any type is not working properly", expected.equals(actual));        
     }
 
     @Test
-
-    public void test2ExecutiveChairs(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+    public void testMaxExecutiveChairs(){
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(400);
-        expected.add(10000);
 
-        testSQL.populateFurniture("chair", "Executive", 2); 
+        testSQL.populateFurniture("chair", "Executive", 1);
+        System.out.println(testSQL.chain.getOverflow());  
         Vector<Integer> actual = testSQL.chain.getPrice();
 
         assertTrue("Getting 2 executive chairs of any type is not working properly", expected.equals(actual));        
     }
 
     @Test
-
-    public void test2ErgonomicChairs(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+    public void testMaxErgonomicChairs(){
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(250);
-        expected.add(10000);
 
-        testSQL.populateFurniture("chair", "Ergonomic", 2); 
+        testSQL.populateFurniture("chair", "Ergonomic", 1);
+        System.out.println(testSQL.chain.getOverflow());  
         Vector<Integer> actual = testSQL.chain.getPrice();
 
         assertTrue("Getting 2 ergonomic chairs of any type is not working properly", expected.equals(actual));        
     }
 
-    @Test
-
-    public void test2KneelingChairs(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
-        testSQL.initializeConnection();
-        Vector<Integer> expected = new Vector<>();
-        expected.add(10000);
-
-        testSQL.populateFurniture("chair", "Kneeling", 2); 
-        Vector<Integer> actual = testSQL.chain.getPrice();
-
-        assertTrue("Getting 2 kneeling chairs of any type is not working properly", expected.equals(actual));        
-    }
 
     @Test
     public void testMaxAdjustableDesks(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(400);
@@ -195,14 +182,13 @@ public class InventoryTest {
 
     @Test
     public void testMaxStandingDesks(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(300);
         expected.add(300);
-        expected.add(10000);
 
-        testSQL.populateFurniture("desk", "Standing", 3); 
+        testSQL.populateFurniture("desk", "Standing", 2); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
         assertTrue("Getting 1 chair of any type is not working properly", expected.equals(actual));        
@@ -210,14 +196,13 @@ public class InventoryTest {
 
     @Test
     public void testMaxTraditionalDesks(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(100);
         expected.add(125);
 
-        
-        testSQL.populateFurniture("desk", "Traditional", 3);
+        testSQL.populateFurniture("desk", "Traditional", 2);
         System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
@@ -226,14 +211,13 @@ public class InventoryTest {
 
     @Test
     public void testMaxSmallFilings(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(100);
         expected.add(125);
-        expected.add(10000);
 
-        testSQL.populateFurniture("filing", "Small", 3);
+        testSQL.populateFurniture("filing", "Small", 2);
         System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
@@ -242,14 +226,14 @@ public class InventoryTest {
 
     @Test
     public void testMaxMediumFilings(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(200);
         expected.add(200);
         expected.add(200);
 
-        testSQL.populateFurniture("filing", "Medium", 4);
+        testSQL.populateFurniture("filing", "Medium", 3);
         System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
@@ -258,14 +242,13 @@ public class InventoryTest {
 
     @Test
     public void testMaxLargeFilings(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(300);
         expected.add(300);
-        expected.add(10000);
 
-        testSQL.populateFurniture("filing", "Large", 3);
+        testSQL.populateFurniture("filing", "Large", 2);
         System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
@@ -274,15 +257,14 @@ public class InventoryTest {
 
     @Test
     public void testMaxDeskLamp(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(20);
         expected.add(20);
         expected.add(20);
-        expected.add(10000);
 
-        testSQL.populateFurniture("lamp", "Desk", 4);
+        testSQL.populateFurniture("lamp", "Desk", 3);
         System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
@@ -291,13 +273,13 @@ public class InventoryTest {
 
     @Test
     public void testMaxSwingArmLamp(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(30);
         expected.add(30);
 
-        testSQL.populateFurniture("lamp", "Swing Arm", 4);
+        testSQL.populateFurniture("lamp", "Swing Arm", 2);
         System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
@@ -306,18 +288,83 @@ public class InventoryTest {
 
     @Test
     public void testMaxStudyLamp(){
-        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", "acaicedo", "Am201849538");
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
         testSQL.initializeConnection();
         Vector<Integer> expected = new Vector<>();
         expected.add(10);
         expected.add(10);
-        expected.add(10000);
 
-        testSQL.populateFurniture("lamp", "Study", 3);
+        testSQL.populateFurniture("lamp", "Study", 2);
         System.out.println(testSQL.chain.getOverflow()); 
         Vector<Integer> actual = testSQL.chain.getPrice();
 
         assertTrue("Getting 1 chair of any type is not working properly", expected.equals(actual));        
     }
+
+    @Test
+    public void testOverflowAll(){
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
+        testSQL.initializeConnection();
+        Vector<Boolean> actual = new Vector<>();
+        int overflow = 100;
+
+        testSQL.populateFurniture("chair", "Task", overflow); 
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("chair", "Kneeling", overflow);
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("chair", "Executive", overflow);
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("chair", "Ergonomic", overflow);
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("chair", "Mesh", overflow);
+        actual.add(testSQL.chain.getOverflow());
+
+        testSQL.populateFurniture("desk", "Traditional", overflow); 
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("desk", "Adjustable", overflow);
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("desk", "Standing", overflow);
+        actual.add(testSQL.chain.getOverflow());
+
+        testSQL.populateFurniture("filing", "Small", overflow); 
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("filing", "Medium", overflow);
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("filing", "Large", overflow);
+        actual.add(testSQL.chain.getOverflow());
+
+        testSQL.populateFurniture("lamp", "Desk", overflow); 
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("lamp", "Study", overflow);
+        actual.add(testSQL.chain.getOverflow());
+        testSQL.populateFurniture("lamp", "Swing Arm", overflow);
+        actual.add(testSQL.chain.getOverflow());
+
+        Vector<Boolean> expected = new Vector<>();
+        for (int i = 0; i < actual.size(); i++){
+            expected.add(true);
+        }
+        assertTrue("Testing overflow does not yeild expected result", expected.equals(actual)); 
+    }
+
+    @Test
+    public void testDeleteFromDatabase(){
+
+        Inventory testSQL = new Inventory("jdbc:mysql://localhost:3306/INVENTORY", USERNAME, PASSWORD);
+        testSQL.initializeConnection();
+
+        testSQL.populateFurniture("chair", "Task", 1);
+        int before = testSQL.chain.getIDVector().size();
+
+        for (int i = 0; i < testSQL.chain.getIDVector().size(); i++){
+            testSQL.deleteFurniture(testSQL.chain.getIDVector().get(i));
+        }
+
+        testSQL.populateFurniture("chair", "Task", 1);
+        int after = testSQL.chain.getIDVector().size();
+
+        assertFalse("Delete function not working properly", before == after);
+    }
+    
 
 }
